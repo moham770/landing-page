b1801816be1edbfccec2d7e30c338421dafdc87f
Ayman <mohamayman770@gmail.com>
2023-07-13 23:24:31 +0300

    frist-commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let header =document.querySelector("header")
+let main_pic =document.querySelector(".immg")
+
+function photoes(creatine){
+    main_pic.src =creatine
+}
+function colors(color){
+    header.style.backgroundColor=(color)
+}
